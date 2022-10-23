61b32c8a866ae2fe8ca45fe60c72e86195570c36
Connor Slade <connor@connorcode.com>
2022-10-22 23:04:34 -0400

    Icon

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use std::path::PathBuf;
+
+use afire::{extension::ServeStatic, Middleware, Server};
+
+fn main() {
+    if !PathBuf::from("web/dist").exists() {
+        println!("[-] Web dist not built!");
+        return;
+    }
+
+    let mut server = Server::<()>::new("localhost", 8080);
+    ServeStatic::new("web/dist").attach(&mut server);
+
+    server.start().unwrap()
+}
