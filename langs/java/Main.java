19d446fc24214128f4651acdaa111934f49ddf42
Connor Slade <connor@connorcode.com>
2022-10-25 23:51:36 -0400

    Java runner progress

--- langs/java/Main.java
+++ langs/java/Main.java
@@ -1,19 +1,107 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 class Main {
-  public static void main(String[] args) throws Exception {
-    String raw = "j2h5hd;ggm;\"mia\",\"egg\">\"egg mia\"";
-    
-    String[] data = raw.split(";");
-    String sharedToken = data[0];
-    String funcName = data[1];
+    public static void main(String[] args) throws Exception {
+        String raw = "j2h5hd;ggm;2,\"egg\">\"egg egg\";3,\"cool\">\"cool cool cool\"";
 
-    Object instance = Solution.class.getDeclaredConstructor().newInstance();
-    Object output = Solution.class.getMethod(funcName).invoke(instance);
-    System.out.println(output);
-  }
+        String[] data = raw.split(";");
+        String sharedToken = data[0];
+        String funcName = data[1];
+        List<List<Object>> cases = extractCases(raw.substring(sharedToken.length() + funcName.length() + 2)).stream()
+                .map(Main::parseCase)
+                .toList();
 
-  static String splitn(String inp, String sub, int count) {
-    for (int i = 0; i < inp.length() - sub.length()) {
-      // if ()
+        int i = 0;
+        for (List<Object> _case : cases) {
+            Object correctOutput = _case.remove(cases.size());
+            Object instance = Solution.class.getDeclaredConstructor()
+                    .newInstance();
+            Object output = Solution.class.getMethods()[0]
+                    .invoke(instance, _case.toArray());
+
+            System.out.print("Case #" + i++ + " (" + output.equals(correctOutput) + ") | ");
+            System.out.println(output);
+        }
     }
-  }
+
+    // TODO: Combine extractCases with parseCase
+    static List<String> extractCases(String raw) {
+        List<String> out = new ArrayList<>();
+        StringBuilder working = new StringBuilder();
+        boolean inString = false;
+
+        for (char i : raw.toCharArray()) {
+            if (i == '"')
+                inString ^= true;
+
+            if (i == ';' && !inString) {
+                out.add(working.toString());
+                working = new StringBuilder();
+                continue;
+            }
+
+            working.append(i);
+        }
+
+        if (!working.isEmpty()) out.add(working.toString());
+        return out;
+    }
+
+    static List<Object> parseCase(String raw) {
+        List<Object> out = new ArrayList<>();
+        StringBuilder working = new StringBuilder();
+        boolean inString = false;
+
+        for (char i : raw.toCharArray()) {
+            if (i == '"')
+                inString ^= true;
+
+            if (i == ',' || i == '>' && !inString) {
+                out.add(parseType(working.toString()).orElseThrow());
+                working = new StringBuilder();
+                continue;
+            }
+
+            working.append(i);
+        }
+
+        if (!working.isEmpty()) {
+            out.add(parseType(working.toString()).orElseThrow());
+        }
+        return out;
+    }
+
+    // TODO: Finish types
+    static Optional<Object> parseType(String raw) {
+        raw = raw.trim();
+
+        // String
+        if (raw.startsWith("\"") && raw.endsWith("\"")) {
+            return Optional.of(raw.substring(1, raw.length() - 1));
+        }
+
+        // Bool
+        if (raw.equals("true")) return Optional.of(true);
+        if (raw.equals("false")) return Optional.of(false);
+
+        // Int
+        try {
+            return Optional.of(Integer.parseInt(raw));
+        } catch (NumberFormatException ignored) {
+        }
+
+        // Float
+        try {
+            return Optional.of(Float.parseFloat(raw));
+        } catch (NumberFormatException ignored) {
+        }
+
+        // Array
+
+        // List
+
+        return Optional.empty();
+    }
 }
