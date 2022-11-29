99ebf008077d71dae2f2ad833e32826eaf433dac
Connor Slade <connor@connorcode.com>
2022-11-28 21:43:06 -0500

    Base svelte

---
+++ web/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  // @ts-nocheck
+  import { Route } from "tinro";
+</script>
+
+<main>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/portfolio">Portfolio</a>
+    <a href="/contacts">Contacts</a>
+  </nav>
+
+  <Route path="/">
+    <h1>This is the main page</h1>
+  </Route>
+  <Route fallback>Page not found!?</Route>
+</main>
