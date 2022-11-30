2a48e5baa15fc3e8a1200f88ebc2079ba905b841
Connor Slade <connor@connorcode.com>
2022-11-30 06:34:54 -0500

    Welcome message

--- web/src/pages/Home.svelte
+++ web/src/pages/Home.svelte
@@ -1,10 +1,42 @@
 <script>
   import Content from "../lib/Content.svelte";
   import Header from "../lib/Header.svelte";
+  import { state } from "../state";
+
+  let user = null;
+  state.getUser().then((u) => (user = u));
 </script>
 
 <Header page={"home"} />
 
 <Content>
-  <h1 class="font-bold underline">This is the main page</h1>
+  {#if !user}
+    <p>Welcome to Coding Hat!</p>
+    <p>
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      You can
+      <span class="link" on:click={() => (location.pathname = "/auth/redirect")}
+        >login with google</span
+      >
+      to get started!
+    </p>
+  {:else}
+    <h1>
+      {user.new
+        ? `Welcome to Codeing Hat, ${user.name}`
+        : `Welcome back, ${user.name}`}
+    </h1>
+    <p>Why not try an <a class="link" href="/p/200079">example problem</a>!</p>
+  {/if}
+
+  <br />
+  <p>This homepage is currently unfinished.</p>
 </Content>
+
+<style>
+  .link {
+    color: rgb(14 165 233);
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
